1221135c0ee12edbfff2107be75e13eff8f8d73b
Christian-Fjord <fjord.christian@gmail.com>
2019-05-30 22:10:35 -0500

    Added addRecipe and getRecipeById endpoints and modules

--- recipes/recipes-model.js
+++ recipes/recipes-model.js
@@ -1,9 +1,21 @@
 const db = require('../data/dbConfig');
 
 module.exports = {
-    getRecipes
+    getRecipes,
+    getRecipeById,
+    addRecipe
 };
 
 function getRecipes() {
     return db('recipes');
 } 
+
+function getRecipe(id) {
+  return db('recipes').where({ id }).first();
+} 
+
+function addRecipe(recipe) {
+  return db('recipes')
+    .insert(recipe)
+    .then(ids => ({ id: ids[0] }));
+}
--- recipes/recipes-router.js
+++ recipes/recipes-router.js
@@ -1,6 +1,9 @@
 const router = require('express').Router();
 const Recipes = require('./recipes-model');
 
+
+// getRecipes
+
 router.get('/', (req, res) => {
     const errorMessage = { message: 'Unable to get recipes' };
 
@@ -10,5 +13,43 @@ router.get('/', (req, res) => {
         })
         .catch(error => { res.status(500).json(errorMessage); });
 });
+
+// getRecipeById
+
+router.get('/:id', (req, res) => {
+  const errorMessage = { message: 'Unable to get recipe' };
+
+  Recipes
+      .getRecipeById(req.params.id)
+      .then(dish => {
+          dish
+              ? res.status(200).json(dish)
+              : res.status(404).json(errorMessage);
+      })
+      .catch(err => {
+          res.status(500).json(errorMessage)
+      })
+});
+
+// addRecipe
+
+router.post('/' (req, res) => {
+  const { recipeName, dishId, instructions } = req.body;
+
+  const errorMessage = { message: 'Unable to create recipe' };
+
+  if (recipeName, dishId, instructions) {
+    Recipes.addRecipe(req.body)
+      .then( recipe => {
+        res.status(201).json(recipe)
+      })
+      .catch( err => {
+        res.status(500).json(errorMessage)
+      })
+  } else {
+    res.status(400).json(errorMessage);;
+  }
+
+})
 
 module.exports = router; 
